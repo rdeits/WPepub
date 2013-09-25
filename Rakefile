336fca2f680ec357ea3ca93d4e24011a04b1c1d7
Robin Deits <robin.deits@gmail.com>
2013-09-24 22:43:05 -0400

    Convert to scripts and complete the full epub conversion

--- Rakefile
+++ Rakefile
@@ -1,7 +1,19 @@
-file "build/out.epub" => FileList['rst/*.rst'] do
+file "build/out.epub" => FileList["rst/01.01_Gestation_1.1.rst"] do
 	mkdir_p "build"
 	sh 'echo "Worm.\n=====\n\nparahumans.wordpress.com\n------------------------\n" > rst/0.0_Title.rst'
 	sh "txt2epub build/out.epub rst/*.rst --title=Worm --creator=parahumans.wordpress.com"
+end
+
+
+mirror_fname = "mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html"
+file mirror_fname do
+	chdir "WPepub"
+	sh "python scrape.py"
+end
+
+file "rst/01.01_Gestation_1.1.rst" => [mirror_fname] do
+	chdir "WPepub"
+	sh "python convert.py"
 end
 
 task :compile => ["build/out.epub"]
--- WPepub/convert.py
+++ WPepub/convert.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from collections import namedtuple
 from unidecode import unidecode
 
-def WP_scrape():
+def WP_convert():
     Chapter = namedtuple('Chapter', ['title', 'url', 'content'])
 
     def html2rst(html):
@@ -14,7 +14,8 @@ def WP_scrape():
         return unidecode(p.communicate(html)[0].decode('utf-8'))
 
     # Pull down a single episode in order to extract the TOC
-    page_content = subprocess.check_output(['curl', 'http://parahumans.wordpress.com/category/stories-arcs-1-10/arc-4-shell/4-x-interlude/'])
+    with open("../mirror/category/stories-arcs-1-10/arc-1-gestation/1-01/index.html", 'r') as f:
+        page_content = f.read()
 
     soup = BeautifulSoup(page_content)
 
@@ -27,26 +28,28 @@ def WP_scrape():
         if li.li is None:
             print li.a.contents[0], li.a.get('href')
             url = li.a.get('href')
-            mirror_path = os.path.join(url.replace('http://parahumans.wordpress.com', '../mirror'),'index.html')
+            mirror_path = os.path.join(url.replace('http://parahumans.wordpress.com', '../mirror'), 'index.html')
             mirror_path = mirror_path.encode('ascii', 'ignore')
-            os.system('mkdir -p ' + os.path.split(mirror_path)[0])
-            with open(mirror_path, 'w') as f:
-                f.write(subprocess.check_output(['curl', url]))
-
-            # chapters.append(Chapter(title=li.a.contents[0], url=li.a.get('href'), content=''))
+            if 'arc-29' in mirror_path:
+                break
+            with open(mirror_path, 'r') as f:
+                content = BeautifulSoup(f.read())
+            chapters.append(Chapter(title=li.a.contents[0], url=url, content=content))
 
     # chapters = [Chapter(title=c.title, url=c.url, content=BeautifulSoup(subprocess.check_output(['curl', c.url]))) for c in chapters]
 
-    # for c in chapters:
-    #     display_title = unidecode(c.content.find_all(attrs={'class':'entry-title'})[0].get_text())
-    #     fname = c.title + ' ' + display_title
-    #     fname = fname.replace(' ', '_')
-    #     f = open('../rst/' + fname + '.rst', 'w')
-    #     f.write("{}\n{}\n".format(display_title,'='*(len(display_title))))
-    #     text = html2rst(str(c.content.find_all("div", attrs={'class':'entry-content'})[0]))
-    #     text = '\n'.join(text.splitlines()[1:-1])
-    #     f.write(text)
-    #     f.close()
+    for c in chapters:
+        display_title = unidecode(c.content.find_all(attrs={'class':'entry-title'})[0].get_text())
+        fname = c.title + ' ' + display_title
+        fname = fname.replace(' ', '_').replace('/', '-')
+        if len(fname.split('.')[0]) == 1:
+            fname = '0' + fname
+        f = open('../rst/' + fname + '.rst', 'w')
+        f.write("{}\n{}\n".format(display_title,'='*(len(display_title))))
+        text = html2rst(str(c.content.find_all("div", attrs={'class':'entry-content'})[0]))
+        text = '\n'.join(text.splitlines()[1:-1])
+        f.write(text)
+        f.close()
 
 if __name__ == '__main__':
-    WP_scrape()
+    WP_convert()
